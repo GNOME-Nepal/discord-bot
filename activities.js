b38bf43a30a063615b8bf43371a4fef3ba9a1551
Prarambha369 <prarambhabashyal@gmail.com>
2025-03-31 12:34:29 +0545

    feat: update activities and configuration files for GNOME Nepal community engagement

--- activities.js
+++ activities.js
@@ -10,7 +10,7 @@
  * Available types: PLAYING, LISTENING, WATCHING, COMPETING, STREAMING
  */
 module.exports = [
-    {name: '$sudo help', type: 'PLAYING'},
+    {name: '@GNOME Nepal', type: 'PLAYING'},
     {name: 'GNOME Nepal Community', type: 'PLAYING'},
     {name: 'Community Outreach', type: 'WATCHING'},
     {name: 'Local Community Engagement', type: 'WATCHING'},
@@ -23,5 +23,14 @@ module.exports = [
     {name: 'Collaborative Projects', type: 'LISTENING'},
     {name: 'International Collaboration', type: 'PLAYING'},
     {name: 'Workshops and Training', type: 'LISTENING'},
-
+    {name: 'GNOME Nepal Discord', type: 'PLAYING'},
+    {name: 'GNOME Nepal Community', type: 'PLAYING'},
+    {name: 'Community Engagement', type: 'LISTENING'},
+    {name: 'GNOME Nepal Events', type: 'LISTENING'},
+    {name: 'GNOME Nepal Projects', type: 'PLAYING'},
+    {name: 'GNOME Nepal Activities', type: 'PLAYING'},
+    {name: 'GNOME Nepal Initiatives', type: 'PLAYING'},
+    {name: 'GNOME Nepal Collaborations', type: 'PLAYING'},
+    {name: 'GNOME Nepal Outreach', type: 'PLAYING'},
+    {name: 'Contributing to GNOME', type: 'PLAYING'},
 ];
