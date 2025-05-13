d9d2e32826175ec45901fc5f6c32e4cfbdf3d65d
mrbashyal <prarambhabashyal@gmail.com>
2025-05-13 17:57:35 +0545

    feat(Guess): differentiate between correct and incorrect guess emojis

--- Event/Ubucon-asia/Guess.js
+++ Event/Ubucon-asia/Guess.js
@@ -38,7 +38,8 @@ const axios = require('axios');
 
 const eventConfig = require('../event.json');
 const MONITORED_CHANNEL_ID = eventConfig.Channel_id;
-const REACTION_EMOJI = '<:gnome:1342508917560971325>';
+const INCORRECT_GUESS_EMOJI = '<:gnome:1342508917560971325>';
+const CORRECT_GUESS_EMOJI = '<:gnome:1342508917560971325>';
 const MASCOT = eventConfig.Mascot.toLowerCase();
 
 // API URL for animal search
@@ -238,6 +239,7 @@ async function searchAnimal(searchTerm) {
         'zoology',
         'wildlife',
         'fauna',
+        'anime',
       ];
 
       // Check the top 3 results (or fewer if there are less than 3)
@@ -547,7 +549,7 @@ async function handleMessage(message) {
         await logChannel.send({ embeds: [logEmbed] });
       }
 
-      await message.react(isCorrectGuess ? '✅' : REACTION_EMOJI);
+      await message.react(isCorrectGuess ? CORRECT_GUESS_EMOJI : INCORRECT_GUESS_EMOJI);
     } else {
       await message.delete();
       const reply = await message.channel.send({
